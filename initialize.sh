78ced75e6d8c2d265ce10dd2ee6411f790e1ddbe
Minal Baghele <94394869+MinalBaghele@users.noreply.github.com>
2024-07-03 18:39:19 +0530

    initialize.sh

---
+++ initialize.sh
@@ -0,0 +1,10 @@
+Frontend (React.js)
+Initialize a new React project using Create React App:
+npx create-react-app movie-booking-system
+cd movie-booking-system
+
+Backend (Node.js + Express)
+Initialize a new Node.js project:
+mkdir backend
+cd backend
+npm init -y
